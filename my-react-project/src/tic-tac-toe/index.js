1adbf9e2cf59f74b33b830614d565e2bd405206f
jie_liu2 <jie_liu2@sui.com>
2022-05-15 17:47:30 +0800

    完成井字棋

--- my-react-project/src/tic-tac-toe/index.js
+++ my-react-project/src/tic-tac-toe/index.js
@@ -1,3 +1,8 @@
+import React from 'react'
+// import ReactDOM from 'react-dom/client'
+import './index.css'
+
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -11,9 +16,9 @@ class Board extends React.Component {
     return (
       <Square
         value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
+        onClick={() => this.props.onClick(i)} 
       />
-    );
+    )
   }
 
   render() {
@@ -41,48 +46,42 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null)
-        }
-      ],
+      history: [{
+        squares: Array(9).fill(null)
+      }],
       stepNumber: 0,
-      xIsNext: true
-    };
+      xIsNext: true,
+    }
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1];
-    const squares = current.squares.slice();
+    const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) {
-      return;
+      return
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([
-        {
-          squares: squares
-        }
-      ]),
+      history: history.concat({squares}),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
-    });
+    })
   }
 
   jumpTo(step) {
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0
+      xIsNext: (step % 2) === 0,
     });
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber]; // 响应式数据更新视图
-    const winner = calculateWinner(current.squares);
+    const history = this.state.history
+    const current = history[this.state.stepNumber];
+    let winner = calculateWinner(current.squares)
 
     const moves = history.map((step, move) => {
       const desc = move ?
@@ -92,22 +91,20 @@ class Game extends React.Component {
         <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
-      );
-    });
-
-    let status;
+      )
+    })
+    let status = ''
     if (winner) {
-      status = "Winner: " + winner;
+      status = 'Winner: ' + winner;
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-
     return (
       <div className="game">
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={i => this.handleClick(i)}
+            onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
@@ -119,11 +116,6 @@ class Game extends React.Component {
   }
 }
 
-// ========================================
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
-
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -133,13 +125,22 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-  ];
+    [2, 4, 6],
+  ]
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+    if (squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
     }
   }
-  return null;
+  return null
 }
+
+// ========================================
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Game />);
+
+
+
+export default Game
--- node.md
+++ node.md
@@ -8,6 +8,10 @@
 
 ## 特性
 1. 函数、类(React.Component) 都能作为组件使用
+   函数组件：不包含state,接收props作为参数, 返回需要渲染的元素。
+   类组件：
 2. setState()更新数据
 3. html能当做data变量使用
-4. 
+4. 可变对象、改变对象
+不可变对象, 是否发生改变，从而确定何时对组件重新渲染。
+性能优化：prue components shouldComponentsUpdate()
