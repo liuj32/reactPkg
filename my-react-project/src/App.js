29ac7c7adc561c8b5490d4c5dfe86cf2d11e70ff
jie_liu2 <jie_liu2@sui.com>
2022-05-16 00:36:52 +0800

    核心概念--条件渲染

--- my-react-project/src/App.js
+++ my-react-project/src/App.js
@@ -1,11 +1,80 @@
 import React from 'react'
 // import ReactDOM from 'react-dom/client'
 
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true};
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate')
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }  
+}
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isToggleOn: true,
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(ev) {
+    this.setState({
+      isToggleOn: !this.state.isToggleOn,
+    })
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>asdf</h1>
+        <Page/>
+        <Toggle/>
       </div>
     );
   }
--- node.md
+++ node.md
@@ -10,8 +10,23 @@
 1. 函数、类(React.Component) 都能作为组件使用
    函数组件：不包含state,接收props作为参数, 返回需要渲染的元素。
    类组件：
-2. setState()更新数据
-3. html能当做data变量使用
-4. 可变对象、改变对象
-不可变对象, 是否发生改变，从而确定何时对组件重新渲染。
-性能优化：prue components shouldComponentsUpdate()
+2. JSX：类似于xml，是一个javascirpt语法扩展，可当做变量赋值.
+   React DOM 使用camelCase来定义属性名称，而不是html属性名称的命名约定。例如：class变为className.
+3. setState()更新数据
+4. 组件生命周期：
+  componentDidMount：组件已经被渲染到DOM中后运行
+  componentDidUpdate：组件发生更新
+  componentWillUnmount：组件从 DOM 中被移除
+
+5. 事件
+   jsx绑定事件：
+   1. () => this.handleClick(),重新渲染会导致创建不同的回调函数
+   2. this.handleClick = this.handleClick().bind(this)
+
+6. 
+
+
+
+<!-- 6. 可变对象、改变对象
+   不可变对象, 是否发生改变，从而确定何时对组件重新渲染。
+   性能优化：prue components shouldComponentsUpdate() -->
